f0b5a112522c0d35136f52911af69a8976f47546
Sergi Granell <xerpi.g.12@gmail.com>
2015-03-23 00:28:10 +0100

    Improved documentation

--- sf2dlib/html/search/functions_0.js
+++ sf2dlib/html/search/functions_0.js
@@ -1,14 +1,21 @@
 var searchData=
 [
+  ['sf2d_5fbind_5ftexture',['sf2d_bind_texture',['../sf2d_8h.html#a5acbbcbe3d4aae913976d01302951e93',1,'sf2d.h']]],
+  ['sf2d_5fcreate_5ftexture',['sf2d_create_texture',['../sf2d_8h.html#aa2d2bf527f988eea5763f122a69c83c5',1,'sf2d.h']]],
   ['sf2d_5fdraw_5fline',['sf2d_draw_line',['../sf2d_8h.html#ace95578f2bfe9f2e8b1753bdd816d96b',1,'sf2d.h']]],
   ['sf2d_5fdraw_5frectangle',['sf2d_draw_rectangle',['../sf2d_8h.html#aec970054d30e326ab9bc616ecab6aff9',1,'sf2d.h']]],
+  ['sf2d_5fdraw_5ftexture',['sf2d_draw_texture',['../sf2d_8h.html#a09501d8d7c5319db00a39709f0cd18fa',1,'sf2d.h']]],
+  ['sf2d_5fdraw_5ftexture_5frotate',['sf2d_draw_texture_rotate',['../sf2d_8h.html#affc6cf46e403ae0837ec2901c85b4464',1,'sf2d.h']]],
   ['sf2d_5fend_5fframe',['sf2d_end_frame',['../sf2d_8h.html#ab933b16f700958c78bd159c997b9124f',1,'sf2d.h']]],
+  ['sf2d_5ffill_5ftexture_5ffrom_5frgba8',['sf2d_fill_texture_from_RGBA8',['../sf2d_8h.html#ac06fd9ba922f84e9f50cac3ac0e5b5a4',1,'sf2d.h']]],
   ['sf2d_5ffini',['sf2d_fini',['../sf2d_8h.html#a322d93586a0d8dd3e0ccd5e23352e8e8',1,'sf2d.h']]],
+  ['sf2d_5ffree_5ftexture',['sf2d_free_texture',['../sf2d_8h.html#a7cda3052688d26cdf0b238d4da1be74a',1,'sf2d.h']]],
   ['sf2d_5finit',['sf2d_init',['../sf2d_8h.html#aa14ff4b0b0b65e43d45bb838f90ff64e',1,'sf2d.h']]],
   ['sf2d_5fpool_5fmalloc',['sf2d_pool_malloc',['../sf2d_8h.html#a62fe88879d163c0c69095944bd29a1b3',1,'sf2d.h']]],
   ['sf2d_5fpool_5fmemalign',['sf2d_pool_memalign',['../sf2d_8h.html#a0d73cc225df3c071ef63dc4cb2a68edc',1,'sf2d.h']]],
   ['sf2d_5fpool_5freset',['sf2d_pool_reset',['../sf2d_8h.html#aaaf6fc32b6d321adbffb78300c7df51e',1,'sf2d.h']]],
   ['sf2d_5fset_5fclear_5fcolor',['sf2d_set_clear_color',['../sf2d_8h.html#a21914ce31f435615df6c743f129d7221',1,'sf2d.h']]],
   ['sf2d_5fstart_5fframe',['sf2d_start_frame',['../sf2d_8h.html#aebf0bffc5ec4230ba44ba6daeddcd040',1,'sf2d.h']]],
-  ['sf2d_5fswapbuffers',['sf2d_swapbuffers',['../sf2d_8h.html#a88b5ab9a86f26417e04ad0305c8bddbd',1,'sf2d.h']]]
+  ['sf2d_5fswapbuffers',['sf2d_swapbuffers',['../sf2d_8h.html#a88b5ab9a86f26417e04ad0305c8bddbd',1,'sf2d.h']]],
+  ['sf2d_5ftexture_5ftile32',['sf2d_texture_tile32',['../sf2d_8h.html#ab485210364f2285c265ec161196695b9',1,'sf2d.h']]]
 ];
