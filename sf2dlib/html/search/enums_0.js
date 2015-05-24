7964212231dd35c4e0f2ea9242c53c6548b11b72
Sergi Granell <xerpi.g.12@gmail.com>
2015-05-24 12:18:42 +0200

    Updated ..

--- sf2dlib/html/search/enums_0.js
+++ sf2dlib/html/search/enums_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['sf2d_5fplace',['sf2d_place',['../sf2d_8h.html#a07951fad547d423a4cf6521c2e5171ea',1,'sf2d.h']]]
+  ['sf2d_5fplace',['sf2d_place',['../sf2d_8h.html#a07951fad547d423a4cf6521c2e5171ea',1,'sf2d.h']]],
+  ['sf2d_5ftexfmt',['sf2d_texfmt',['../sf2d_8h.html#a159c44ac8ef84cd08235063d4e220244',1,'sf2d.h']]]
 ];
--- sf2dlib/html/search/variables_6.js
+++ sf2dlib/html/search/variables_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['pixel_5fformat',['pixel_format',['../structsf2d__texture.html#a247bd65d7ab9db17fdd8bd78b17cba23',1,'sf2d_texture']]],
+  ['pixel_5fformat',['pixel_format',['../structsf2d__texture.html#a1292c9a846b8593163bd3c1eaeb459c2',1,'sf2d_texture']]],
   ['place',['place',['../structsf2d__texture.html#ac30cfc2de337739d1a19858e4fdacaa4',1,'sf2d_texture']]],
   ['position',['position',['../structsf2d__vertex__pos__col.html#a72c48b1c976984ef0512a28e8af7a1e4',1,'sf2d_vertex_pos_col::position()'],['../structsf2d__vertex__pos__tex.html#ae5e85779f0eb3db4b2c26fe77be8cf41',1,'sf2d_vertex_pos_tex::position()']]],
   ['pow2_5fh',['pow2_h',['../structsf2d__texture.html#a644c43b5fde6e12c8f1e3cb0280798e1',1,'sf2d_texture']]],
